ff920267b502d00590b038366eabc865bfc6cf26
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2024-12-11 00:45:34 +0530

    refactor: trim down unused and unneeded code

--- src/single_drone.py
+++ src/single_drone.py
@@ -2,29 +2,20 @@
 Modified from the `pid_velocity.py` example from gym-pybullet-drones
 """
 
-import os
 import time
-import argparse
-from datetime import datetime
-import pdb
-import math
-import random
+# import argparse
 import numpy as np
-import pybullet as p
-import matplotlib.pyplot as plt
 
 from gym_pybullet_drones.utils.enums import DroneModel, Physics
-from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
-from gym_pybullet_drones.control.DSLPIDControl import DSLPIDControl
 from gym_pybullet_drones.utils.Logger import Logger
-from gym_pybullet_drones.utils.utils import sync, str2bool
+from gym_pybullet_drones.utils.utils import sync
 
 from gym_pybullet_drones.envs.VelocityAviary import VelocityAviary
 
 DEFAULT_DRONE = DroneModel("cf2x")
 DEFAULT_GUI = True
 DEFAULT_RECORD_VIDEO = False
-DEFAULT_PLOT = True
+DEFAULT_PLOT = False
 DEFAULT_USER_DEBUG_GUI = False
 DEFAULT_OBSTACLES = False
 DEFAULT_SIMULATION_FREQ_HZ = 240
@@ -34,19 +25,19 @@ DEFAULT_OUTPUT_FOLDER = "results"
 DEFAULT_COLAB = False
 
 
-def run(
-    drone=DEFAULT_DRONE,
-    gui=DEFAULT_GUI,
-    record_video=DEFAULT_RECORD_VIDEO,
-    plot=DEFAULT_PLOT,
-    user_debug_gui=DEFAULT_USER_DEBUG_GUI,
-    obstacles=DEFAULT_OBSTACLES,
-    simulation_freq_hz=DEFAULT_SIMULATION_FREQ_HZ,
-    control_freq_hz=DEFAULT_CONTROL_FREQ_HZ,
-    duration_sec=DEFAULT_DURATION_SEC,
-    output_folder=DEFAULT_OUTPUT_FOLDER,
-    colab=DEFAULT_COLAB,
-):
+def run():
+    # use defaults
+    drone = DEFAULT_DRONE
+    gui = DEFAULT_GUI
+    record_video = DEFAULT_RECORD_VIDEO
+    plot = DEFAULT_PLOT
+    user_debug_gui = DEFAULT_USER_DEBUG_GUI
+    obstacles = DEFAULT_OBSTACLES
+    simulation_freq_hz = DEFAULT_SIMULATION_FREQ_HZ
+    control_freq_hz = DEFAULT_CONTROL_FREQ_HZ
+    duration_sec = DEFAULT_DURATION_SEC
+    output_folder = DEFAULT_OUTPUT_FOLDER
+    colab = DEFAULT_COLAB
 
     #### Initialize the simulation #############################
     INIT_XYZS = np.array([[0, 0, 0.1], [0.3, 0, 0.1], [0.6, 0, 0.1], [0.9, 0, 0.1]])
@@ -154,87 +145,9 @@ def run(
 
 if __name__ == "__main__":
     #### Define and parse (optional) arguments for the script ##
-    parser = argparse.ArgumentParser(
-        description="Velocity control example using VelocityAviary"
-    )
-    parser.add_argument(
-        "--drone",
-        default=DEFAULT_DRONE,
-        type=DroneModel,
-        help="Drone model (default: CF2X)",
-        metavar="",
-        choices=DroneModel,
-    )
-    parser.add_argument(
-        "--gui",
-        default=DEFAULT_GUI,
-        type=str2bool,
-        help="Whether to use PyBullet GUI (default: True)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--record_video",
-        default=DEFAULT_RECORD_VIDEO,
-        type=str2bool,
-        help="Whether to record a video (default: False)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--plot",
-        default=DEFAULT_PLOT,
-        type=str2bool,
-        help="Whether to plot the simulation results (default: True)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--user_debug_gui",
-        default=DEFAULT_USER_DEBUG_GUI,
-        type=str2bool,
-        help="Whether to add debug lines and parameters to the GUI (default: False)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--obstacles",
-        default=DEFAULT_OBSTACLES,
-        type=str2bool,
-        help="Whether to add obstacles to the environment (default: False)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--simulation_freq_hz",
-        default=DEFAULT_SIMULATION_FREQ_HZ,
-        type=int,
-        help="Simulation frequency in Hz (default: 240)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--control_freq_hz",
-        default=DEFAULT_CONTROL_FREQ_HZ,
-        type=int,
-        help="Control frequency in Hz (default: 48)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--duration_sec",
-        default=DEFAULT_DURATION_SEC,
-        type=int,
-        help="Duration of the simulation in seconds (default: 5)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--output_folder",
-        default=DEFAULT_OUTPUT_FOLDER,
-        type=str,
-        help='Folder where to save logs (default: "results")',
-        metavar="",
-    )
-    parser.add_argument(
-        "--colab",
-        default=DEFAULT_COLAB,
-        type=bool,
-        help='Whether example is being run by a notebook (default: "False")',
-        metavar="",
-    )
-    ARGS = parser.parse_args()
+    # parser = argparse.ArgumentParser(
+    #     description="Velocity control example using VelocityAviary"
+    # )
+    # ARGS = parser.parse_args()
 
-    run(**vars(ARGS))
+    run()
